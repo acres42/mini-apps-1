5cddf1f106751a0791736252c93d79dcec755241
AC Roselee <acroselee@gmail.com>
2019-01-30 20:43:16 -0800

    Safety commit - added JQuery to packages

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('client'));
+
+app.post('/upload_json', (req, res) => {
+  console.log(req.body);
+});
 
 
+app.listen(port, () => console.log(`Challenge 2 app listening on port ${port}!`));
