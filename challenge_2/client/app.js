e1e76df57e50eebcc6d222251798fddf10c3c622
AC Roselee <acroselee@gmail.com>
2019-01-31 16:13:17 -0800

    BR complete, including download

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,47 @@
+var app = {
+    init: function() {
+      $('#app').append(`<form id='form'></form>`);
+      $('#form').append(`<input type=textarea id='text'>`);
+      $('#form').append(`<button type='submit' id='submit'>Submit</button>`);
+      $('#submit').on('click', app.handleSubmit);
+    },
 
+    handleSubmit: function(event) {
+      var body = {
+        text: $('#text').val()
+      };
+      $.ajax({
+        type: 'POST',
+        data: JSON.stringify(body),
+        contentType: 'application/json',
+        success: function(data) {
+          $('#text').val('');
+          app.handleDownloadButton();
+        }
+      });
+      event.preventDefault();
+    },
+    // https://expressjs.com/en/api.html#res.download
+    //https://stackoverflow.com/questions/24956231/how-to-make-action-on-node-js-server-after-button-click-using-jquery-ajax
+    handleDownloadButton: function() {
+      $('#app').append(`<button type='button' id='dl'>Download Final CSV</button>`);
+      $('#dl').on('click', function(event){
+        $('#dl').detach();
+        $.get("/download", function(file){
+          // https://github.com/mholt/PapaParse/issues/175 - hatip to mholt
+          var blob = new Blob([file]);
+          if (window.navigator.msSaveOrOpenBlob){
+            window.navigator.msSaveBlob(blob, "final.csv");
+          } else {
+            var a = window.document.createElement("a");
+            a.href = window.URL.createObjectURL(blob, {type: "text/plain"});
+            a.download = "final.csv";
+            document.body.appendChild(a);
+            a.click();  // IE: "Access is denied"; see: https://connect.microsoft.com/IE/feedback/details/797361/ie-10-treats-blob-url-as-cross-origin-and-denies-access
+            document.body.removeChild(a);
+          }
+        });
+      });
+    },
+}
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,12 +1,70 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var app = express();
 
-app.use(express.static('client'));
+var port = 3000;
 
-app.post('/upload_json', (req, res) => {
-  console.log(req.body);
+app.use(bodyParser.json());
+
+app.post('/', function(req, res) {
+  fs.open(path.join(__dirname + '/final.csv'), 'w', (err, file) => {
+    if (err) throw err;
+
+    var body = req.body.text;
+    if (body[body.length - 1] === ';') {
+      body = body.slice(0, -1)
+    };
+    var data = JSON.parse(body);
+
+    var sep = ',';
+    var header = '';
+    for (key in data) {
+      if (key !== 'children') {
+        header = header + key + sep;
+      };
+    };
+    header = header.slice(0, -1) + '\n';
+    fs.appendFile('final.csv', header, () => {
+
+      var rows = '';
+      var recurse = function(data) {
+        if (!data.children) {
+          return;
+        }
+        var row = '';
+        for (key in data) {
+          if (key !== 'children') {
+            row = row + data[key] + sep;
+          }
+        }
+        row = row.slice(0, -1) + '\n';
+        rows = rows + row;
+        for (var i = 0; i < data.children.length; i++) {
+          recurse(data.children[i]);
+        }
+      }
+      recurse(data);
+
+
+      fs.appendFile('final.csv', rows, () => {
+        // Send Data Back to Client
+        fs.readFile(path.join(__dirname + '/final.csv'), (err, data) => res.send(data));
+      });
+    });
+  }); //EOF fs.open
+}); //EOF app.post /
+
+app.get('/download', (req, res) => {
+  res.setHeader('Content-disposition', 'attachment; filename=final.csv');
+  res.set('Content-Type', 'text/csv');
+  res.download(__dirname + '/final.csv', 'final.csv')
 });
 
-
+app.use(express.static(path.join(__dirname, 'client')));
 app.listen(port, () => console.log(`Challenge 2 app listening on port ${port}!`));
+
+// The server must flatten the JSON hierarchy, mapping each item/object in the JSON to a single line of CSV report (see included sample output), where the keys of the JSON objects will be the columns of the CSV report.
+// You may assume the JSON data has a regular structure and hierarchy (see included sample file). In other words, all sibling records at a particular level of the hierarchy will have the same set of properties, but child objects might not contain the same properties. In all cases, every property you encounter must be present in the final CSV output.
+// You may also assume that child records in the JSON will always be in a property called `children`.
